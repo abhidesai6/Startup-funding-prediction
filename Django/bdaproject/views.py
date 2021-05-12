d959ac893f6234d45de5775ec95799c46512e06c
Abhishek Desai <55031453+abhidesai6@users.noreply.github.com>
2021-05-12 23:39:26 +0530

    first commit

---
+++ Django/bdaproject/views.py
@@ -0,0 +1,25 @@
+from django.shortcuts import render
+from subprocess import run, PIPE
+import sys
+
+
+def home(request):
+    return render(request, 'home.html')
+
+def about(request):
+    return render(request, 'about.html')
+
+def features(request):
+    return render(request, 'features.html')
+
+def compute(request):
+    rnd = request.POST.get('rnd')
+    admn = request.POST.get('admn')
+    mks = request.POST.get('mks')
+    loc = request.POST.get('loc')
+    indv = request.POST.get('indv')
+
+    
+    out = run([sys.executable, 'S:\\BDA\\demo.py', rnd, admn, mks, loc, indv], shell=False, stdout=PIPE)
+    print(out)
+    return render(request, 'home.html', {'predicted':out.stdout.decode("utf-8")})
